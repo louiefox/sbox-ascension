486c8b819dc216046e30d7ffa0ec12a4f1b58dbe
louiefox <louie.waterfall@hotmail.co.uk>
2021-05-28 09:26:39 +0100

    Initial Files

---
+++ code/Game.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+/// <summary>
+/// This is the heart of the gamemode. It's responsible
+/// for creating the player and stuff.
+/// </summary>
+[Library( "ascension", Title = "Ascension" )]
+partial class DeathmatchGame : Game
+{
+	DeathmatchHud DmHUD;
+	public DeathmatchGame()
+	{
+		//
+		// Create the HUD entity. This is always broadcast to all clients
+		// and will create the UI panels clientside. It's accessible 
+		// globally via Hud.Current, so we don't need to store it.
+		//
+		if ( IsServer )
+		{
+			DmHUD = new DeathmatchHud();
+		}
+	}
+
+	[Event.Hotload]
+	public void UpdateHUD()
+	{
+		if( !IsServer ) { return; }
+		DmHUD.Delete();
+		DmHUD = new DeathmatchHud();
+	}	
+
+	public override void PostLevelLoaded()
+	{
+		base.PostLevelLoaded();
+
+		ItemRespawn.Init();
+	}
+
+	public override void ClientJoined( Client cl )
+	{
+		base.ClientJoined( cl );
+
+		var player = new DeathmatchPlayer();
+		player.Respawn();
+
+		cl.Pawn = player;
+	}
+}
---
+++ code/ui/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+public class MainMenu : Panel
+{
+	private bool IsOpen = false;
+	private float LastToggleTime = 0;
+
+	private Label SkillPoints;
+
+	public MainMenu()
+	{
+		StyleSheet.Load( "/ui/MainMenu.scss" );
+
+		Panel navBar = Add.Panel( "navBar" );
+		navBar.Add.Label( "ARMOURY", "navButton" );
+		navBar.Add.Label( "STATISTICS", "navButton" );
+		navBar.Add.Label( "SOCIAL", "navButton" );
+
+		Panel pageArea = Add.Panel( "pageArea" );
+
+		Panel armouryPage = pageArea.Add.Panel( "armouryPage" );
+
+		Panel slot1 = armouryPage.Add.Panel( "armourySlot" );
+		Panel slot2 = armouryPage.Add.Panel( "armourySlot" );
+		slot2.Style.MarginTop = Length.Percent( 4f );
+
+		SkillPoints = slot2.Add.Label( "Skill Points: 0", "skillPoints" );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if( Local.Client.Input.Pressed( InputButton.Menu ) && Time.Now >= LastToggleTime+.1f )
+		{
+			LastToggleTime = Time.Now;
+			IsOpen = !IsOpen;
+
+			Parent.SetClass( "mainmenuopen", IsOpen );
+		}
+
+		DeathmatchPlayer ply = Local.Pawn as DeathmatchPlayer;
+		if( ply == null ) { return; }
+
+		SkillPoints.Text = $"Skill Points: {ply.SkillPoints}";
+	}
+}
